6d475900383a1c6d98294cda9cbbfab1bd9bcce0
Pavlo Chykalo <P.Chykalo.offer@gmail.com>
2025-06-26 19:39:35 +0300

    feat(Dart_Homework-06):task2

--- 1_task.dart
+++ 1_task.dart
@@ -1,5 +1,6 @@
 void main(){
   task1();
+  task2();
 }
 void task1(){
 double l = 18.6;
@@ -8,4 +9,10 @@ final double P = l*w;
 print('Периметр прямокутника: ${(2*w)+(2*l)}');
 print('Площа прямокутника: $P');
 print('Цілочисельна площа прямокутника: ${P~/1}');
+}
+void task2(){
+  int number = 55;
+  if (number<50) {print('number менше 50!');}
+  else if(number<100) {print('number менше 100!');}
+  else{print('number менше 50, але більше 100');};
 }
